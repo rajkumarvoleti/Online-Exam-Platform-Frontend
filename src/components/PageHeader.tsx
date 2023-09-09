5d02fdcc9e8fe738b6d2d0a40d14045551964862
rajkumarvoleti <voletirajkumar@gmail.com>
2023-09-10 00:07:17 +0530

    page header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,4 +1,5 @@
 import { Card, SxProps } from "@mui/material";
+import { usePathname } from "next/navigation";
 
 const styles:SxProps = {
   margin: 0,
@@ -6,14 +7,25 @@ const styles:SxProps = {
   pl: "10px",
 }
 
-const urls = {
+const urls:{[key: string]: string} = {
   "/user/profile":"Profile",
+  "/user/dashboard":"Dashboard",
+  "/user/create/test/display":"Test",
+  "/user/create/test":"Create test",
+  "/user/create/questionBank":"Create Question bank",
+  "/user/edit/test":"Create test",
+  "/user/edit/questionBank":"Create Question bank",
+  "/user/edit/question":"Create Question bank",
+  "/user/test":"Test",
 }
 
 export default function PageHeader({text}:{text:string}){
+
+  const pathName = usePathname();
+
   return (
     <Card sx={styles} variant="outlined">
-      <h5>{text}</h5>
+      <h5>{urls[pathName] || "Question Bank"}</h5>
     </Card>
   )
 }
